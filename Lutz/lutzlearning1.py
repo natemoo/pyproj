cd847027ac111bff1402e76deafd7567f62cb41c
natemoo <nathanandrewmoore@gmail.com>
2019-12-29 09:11:01 -0700

    Added more to Lutz

--- Lutz/lutzlearning1.py
+++ Lutz/lutzlearning1.py
@@ -1,5 +1,8 @@
-import os #Used in line 9
-import sys #Used in line 12
+import os #Used in line 12
+import sys #Used in line 15
+import threenames #Prints here and used in line 22
+import math #Used on line 26
+import random #Used on line 29
 
 print('Hello World')
 
@@ -13,3 +16,41 @@ print(sys.platform)
 print(2 ** 100)
 x = 'Spam!'
 print(x * 8)
+
+#You can save the output of a file to a file using the > syntax in command line
+
+print(threenames.b, threenames.c)
+print(dir(threenames))
+exec(open('threenames.py').read())
+
+print(math.pi)
+print(math.sqrt(85))
+
+print(random.random())
+print(random.choice([1, 2, 3, 4]))
+
+S = 'shrubbery'
+L = list(S)
+print(L)
+L[1] = 'c'
+print(''.join(L))
+
+B = bytearray(b'spam')
+B.extend(b'eggs')
+print(B)
+print(B.decode())
+
+S = 'Spam'
+print(S.find('pa'))
+print(S.replace('pa', 'XYZ'))
+
+line = 'aaa,bbb,ccc,dd'
+print(line.split(','))
+
+S = 'spam'
+print(S.upper())
+print(S.isalpha())
+
+line = 'aaa,bbb,cccc,dd\n'
+print(line.rstrip())
+print(line.rstrip().split(','))
---
+++ Lutz/lutzlearning2.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python
+
+#Shebang line (above) allows this script to be run directly
+#use chmod +x from terminal to give the file executabe privileges
+#change the shebang line to /usr/bin/python3 for Windows
+
+print('The Bright Side ' + 'of Life...')
