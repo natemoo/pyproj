60260b5903d65e2a709d07440b1620002542cde9
natemoo <nathanandrewmoore@gmail.com>
2019-12-29 08:06:22 -0700

    Added file for Lutz Learning Python Book.

---
+++ Lutz/lutzlearning1.py
@@ -0,0 +1,15 @@
+import os #Used in line 9
+import sys #Used in line 12
+
+print('Hello World')
+
+print(2 ** 8)
+print('Spam' *8)
+
+cwd = os.getcwd()
+print(cwd)
+
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+print(x * 8)
