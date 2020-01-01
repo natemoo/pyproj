5d4466c08b8afa40d7e8472d99719c69a03aa95f
natemoo <nathanandrewmoore@gmail.com>
2019-12-31 22:40:58 -0700

    Lutz update

--- Lutz/lutzlearning1.py
+++ Lutz/lutzlearning1.py
@@ -1,8 +1,9 @@
-import os #Used in line 12
-import sys #Used in line 15
-import threenames #Prints here and used in line 22
-import math #Used on line 26
-import random #Used on line 29
+import os #Used in line 13
+import sys #Used in line 16
+import threenames #Prints here and used in line 23
+import math #Used on line 27
+import random #Used on line 30
+import re #Used on line 66
 
 print('Hello World')
 
@@ -60,3 +61,63 @@ print('{0}, eggs, and {1}'.format('spam', 'SPAM!'))
 print('{}, eggs, and {}'.format('spam', 'SPAM!'))
 print('{:,.2f}'.format(296999.2567))
 print('%.2f | %+05d' % (3.14159, -42))
+
+match = re.match('Hello[ \t]*(.*)world', 'Hello    Python world')
+#match.group(1)
+match = re.match('[/:](.*)[/:](.*)[:/](.*)','/usr/home:lumberjack')
+match.groups()
+print(re.split('[/:]','/usr/home/lumberjack'))
+
+L = [123, 'spam', 1.23]
+print(len(L))
+print(L[0])
+print(L[:-1])
+print(L + [4,5,6])
+print(L * 2)
+print(L) #We didn't change the original list
+L.append('NI')
+print(L)
+L.pop(2)
+print(L)
+M = ['bb', 'aa', 'cc']
+M.sort()
+print(M)
+M.reverse()
+print(M)
+
+M = [[1,2,3],
+[4,5,6],
+[7,8,9]]
+print(M)
+print(M[1])
+print(M[1][2])
+
+#col2 = [row[1] for row in M]
+#print(col2)
+
+#print([row[1] + 1 for row in M])
+#print([row[1] for row in M is row[1] % 2 == 0])
+
+diag = [M[i][i] for i in [0, 1, 2]]
+print(diag)
+doubles = [c * 2 for c in 'spam']
+print(doubles)
+print(list(range(4)))
+print(list(range(-6,7,2)))
+print([[x ** 2, x ** 3] for x in range(4)])
+print([[x, x/2, x * 2] for x in range(-6, 7, 2) if x > 0])
+
+G = (sum(row) for row in M)
+print(next(G))
+print(next(G))
+print(next(G))
+
+print({sum(row) for row in M})
+print({i : sum(M[i]) for i in range(3)})
+
+print([ord(x) for x in 'spaam'])
+print({ord(x) for x in 'spaam'})
+print({x: ord(x) for x in 'spaam'})
+print((ord(x) for x in 'spaam'))
+
+D = {'food' }
